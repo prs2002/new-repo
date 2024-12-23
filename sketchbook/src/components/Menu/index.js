8a8cfc0e1a5522468a85157e62b9a0653f74bf6e
prs2002 <praneetsuyal+github@gmail.com>
2024-12-23 23:33:53 +0530

    Menu component + tailwind configured

--- sketchbook/src/components/Menu/index.js
+++ sketchbook/src/components/Menu/index.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRotateLeft, faRotateRight, faPencil, faEraser, faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
+import styles from './index.module.css'
 
-export const index = () => {
+const menu = () => {
   return (
-    <div>index</div>
+    <div className={styles.menuContainer}>
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faPencil} />
+      </div>
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faRotateLeft} />
+      </div>
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faRotateRight} />
+      </div>
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faEraser} />
+      </div>
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faFileArrowDown} />
+      </div>
+    </div>
   )
 }
+
+export default menu
