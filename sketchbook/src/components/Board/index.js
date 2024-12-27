1096a1956d43bde11fe6adedff716f561249fa88
prs2002 <praneetsuyal+github@gmail.com>
2024-12-27 13:27:01 +0530

    toolbox and menu interaction completed

--- sketchbook/src/components/Board/index.js
+++ sketchbook/src/components/Board/index.js
@@ -1,9 +1,20 @@
 import { useEffect,useRef } from "react"
+import { useSelector } from "react-redux";
 
 const board = () => {
     const canvasRef = useRef(null);
     
+    const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
+    const {color, size} = useSelector((state) => state.toolbox[activeMenuItem])
+    useEffect(() => {
+      if (!canvasRef.current) return
+      const canvas = canvasRef.current;
+      const context = canvas.getContext('2d')
 
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight
+    }, [])
+    console.log(color,size)
   return (
     <canvas ref={canvasRef}>
 
--- sketchbook/src/components/Toolbox/index.js
+++ sketchbook/src/components/Toolbox/index.js
@@ -7,18 +7,16 @@ import {changeColor, changeBrushSize } from '@/slice/toolboxSlice'
 const Toolbox = () => {
     const dispatch = useDispatch()
     const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
+    const {color, size} = useSelector((state) => state.toolbox[activeMenuItem])
     const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL
     const showBrushToolOption = activeMenuItem === MENU_ITEMS.PENCIL || activeMenuItem === MENU_ITEMS.ERASER
-    const {color, size} = useSelector((state) => state.toolbox[activeMenuItem])
 
     const updateBrushSize = (e) => {
         dispatch(changeBrushSize({item: activeMenuItem, size: e.target.value}))
-        socket.emit('changeConfig', {color, size: e.target.value })
-    }
+        }
     
     const updateColor = (newColor) => {
         dispatch(changeColor({item: activeMenuItem, color: newColor}))
-        socket.emit('changeConfig', {color: newColor, size })
     }
 
   return (
@@ -34,12 +32,12 @@ const Toolbox = () => {
                 <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.YELLOW})} style={{backgroundColor: COLORS.YELLOW}} onClick={() => updateColor(COLORS.YELLOW)}/>
             </div>
         </div> }
-        <div className={styles.toolItem}>
+        {showBrushToolOption && <div className={styles.toolItem}>
             <h4 className={styles.toolText}>Brush Size</h4>
             <div className={styles.itemContainer}>
                 <input type="range" min={1} max={10} step={1} onChange={updateBrushSize}/>
             </div>
-        </div>
+        </div>}
     </div>
   )
 }
